796fc3a333fae154f5935dc0ad24338b18af362b
Shubham Upadhyay <shubhamup47@gmail.com>
2023-05-11 21:48:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,17 @@
-// Your Script here.
+function rot13(str) {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
-};
+    const regex = /[a-zA-Z]/g;
+	const decodedStr = str.replace(regex, (char) => {
+    const index = rot13.indexOf(char);
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-    for(let i=0; i<encodedStr.length; i++)
-		{
-			let char = encodedStr[i];
-			if (char.match(/[A-Za-z]/)) {
-				let code = char.charCodeAt(0);
-				if(code >= 65 && code <= 90)
-				{
-					char = String.fromCharCode(((code - 65 + 13) % 26) + 65);
-			    }
-				else if(code >= 97 && code <= 122)
-				{
-					char = String.fromCharCode(((code - 97 + 13) % 26) + 97);
-			    }
-			}
-			decodedArr.push(char);
-		}
-	return decodedArr;//return decodedArr
+    if(index === -1)
+	{
+      return char;
+    }
+		return alphabet[index];
+  });
+  return decodedStr;
 }
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
 module.exports = rot13;
